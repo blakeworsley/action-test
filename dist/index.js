18cdddc82818983f3fa77d6c7e9c70eb56544c65
blakeworsley <blake.worsley@guildeducation.com>
2020-02-10 12:32:34 -0700

    Updates to add clubhouse functionality and update pr

--- dist/index.js
+++ dist/index.js
@@ -50,25 +50,77 @@ const core = __webpack_require__(258);
 const github = __webpack_require__(11);
 const fetch = __webpack_require__(377);
 
+const getStoryIdFromBranch = ref => {
+  return (
+    ref &&
+    ref.includes("/ch") &&
+    ref
+      .split("/")
+      .find(f => f.includes("ch") && parseInt(f.split("ch")[1]))
+      .split("ch")[0]
+  );
+};
+
+const getClubhouseStory = async (storyId, token) => {
+  const result = await fetch(
+    `https://api.clubhouse.io/api/v3/stories/${storyId}?token=${token}`,
+    { method: "GET", redirect: "follow" }
+  );
+  return result.json();
+};
+
+const sanitizeBody = (body, url, title, description) => {
+  // const descriptionHeader = "### What does this pull request do?";
+  const descriptionHeader = core.getInput("descriptionHeader");
+  const descriptionBody = description;
+
+  // const urlHeader = "### What is the relevant story?";
+  const urlHeader = core.getInput("urlHeader");
+  const urlBody = title && url && `[${title}](${url})`;
+
+  const delimiter = core.getInput("delimiter");
+  const bodyParts = body.split(delimiter);
+
+  const newBody = bodyParts.map(part => {
+    if (part.includes(descriptionHeader)) {
+      return `${descriptionHeader}\n${descriptionBody}\n`;
+    }
+    if (part.includes(urlHeader)) {
+      return `${urlHeader}\n${urlBody}\n`;
+    } else {
+      return part;
+    }
+  });
+  return newBody.join(`${delimiter}\n`);
+};
+
+const updatePR = async (url, title, description) => {
+  const github_token = core.getInput("GITHUB_TOKEN", { required: true });
+  const request = {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload.pull_request.number
+  };
+
+  const body = github.context.payload.pull_request.body;
+
+  request.body = sanitizeBody(body, url, title, description);
+  const client = new github.GitHub(github_token);
+  const response = await client.pulls.update(request);
+  core.info(`response: ${response.status}`);
+  console.log("PR was successfully updated");
+};
+
 const run = async () => {
   try {
-    const storyId = core.getInput("storyId");
-    const token = core.getInput("token");
-
-    const result = await fetch(
-      `https://api.clubhouse.io/api/v3/stories/${storyId}?token=${token}`,
-      { method: "GET", redirect: "follow" }
-    );
-
-    const story = await result.json();
-
-    console.log(">>>>>>>>>>> URL <<<<<<<<<<<<", story.app_url);
-    console.log(">>>>>>>>>>> TITLE <<<<<<<<<<<<", story.name);
-    console.log(">>>>>>>>>>> DESCRIPTION <<<<<<<<<<<<", story.description);
-
-    core.setOutput("url", story.app_url);
-    core.setOutput("title", story.name);
-    core.setOutput("description", story.description);
+    const clubhouse_token = core.getInput("CLUBHOUSE_TOKEN");
+    const storyId = await getStoryIdFromBranch(github.context.payload.ref);
+    const story = await getClubhouseStory(storyId, clubhouse_token);
+    if (story) {
+      await updatePR(story.app_url, story.name, story.description);
+    } else {
+      console.log("PR was not updated");
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
